ee8c0ebf335a1a3a3b773ae5151fc3c83696fd91
Piotr Szydywar <pszydywar@gmail.com>
2019-05-11 17:05:12 +0200

    first part of exercise 3 - new post implemented

--- twitter/urls.py
+++ twitter/urls.py
@@ -7,4 +7,5 @@ from twitter import views
 app_name = 'twitter'
 urlpatterns = [
     path('', views.MainWebpageView.as_view(), name="index"),
+    path('compose/', views.TweetComposeView.as_view(), name="compose"),
 ]
--- twitter/views.py
+++ twitter/views.py
@@ -1,13 +1,29 @@
 from django import views
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView
 
 from twitter import models
+from twitter import forms
 
 
 # Create your views here.
 
 class MainWebpageView(views.View):
     def get(self, request):
-        tweets = models.Tweet.objects.all().order_by('-creation_date')
+        tweets = models.Tweet.objects.order_by('-creation_date').all()
         ctx = {'tweets': tweets}
         return render(request, 'twitter/index.html', ctx)
+
+
+class TweetComposeView(LoginRequiredMixin, CreateView):
+    model = models.Tweet
+    form_class = forms.TweetForm
+    success_url = reverse_lazy('twitter:index')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
